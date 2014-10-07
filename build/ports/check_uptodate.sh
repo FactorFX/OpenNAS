a5cc19f7cbe360496eaf16dfb809fe99b6a40d12
Cédric Denis <cedric@factorfx.com>
2014-10-07 10:23:52 +0200

    Change additionnal ports in ovverride mode with /usr/ports

--- build/ports/check_uptodate.sh
+++ build/ports/check_uptodate.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env sh
 
-PAC="bacula-server cmdwatch ipmitool ncdu nmap ocsinventory-agent pciutils pdksh sssd sysinfo vim-lite zsh zfs-stats sysinfo"
+PAC="bacula-server cmdwatch ipmitool ncdu nmap ocsinventory-agent pciutils pdksh sssd sysinfo vim-lite zsh zfs-stats"
 
 for P in $PAC; do
-        OLD_VERSION=$(make -f $P/Makefile -V DISTVERSION)
-        CAT=$(make -f $P/Makefile -V CATEGORIES | awk 'FS=" " {print $1}')
-        PORT="/usr/ports/$CAT/$P"
-        NEW_VERSION=$(make -f $PORT/Makefile -V DISTVERSION)
+	OLD_VERSION=$(make -f $P/Makefile -V DISTVERSION)
+	CAT=$(make -f $P/Makefile -V CATEGORIES | awk 'FS=" " {print $1}')
+	PORT="/usr/ports/$CAT/$P"
+	NEW_VERSION=$(make -f $PORT/Makefile -V DISTVERSION)
 
-        echo "$P $OLD_VERSION -> $NEW_VERSION ($PORT)"
+	echo "$P $OLD_VERSION -> $NEW_VERSION ($PORT)"
 done
