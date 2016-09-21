b0aad55b6d2c51b03466e23975470abc4381fd50
Cédric Denis <cedric@factorfx.com>
2016-09-21 17:54:42 +0200

    Correct indentation patch oce file

--- build/kernel-patches/oce/files/patch-oce_mbox.c
+++ build/kernel-patches/oce/files/patch-oce_mbox.c
@@ -1,10 +1,11 @@
---- sys/dev/oce/oce_mbox.c.orig 2015-06-24 15:51:28.000000000 +0300
-+++ sys/dev/oce/oce_mbox.c      2015-06-24 15:51:32.000000000 +0300
+--- sys/dev/oce/oce_mbox.c.orig	2016-09-21 17:40:59.582021442 +0200
++++ sys/dev/oce/oce_mbox.c	2016-09-21 17:41:20.901017771 +0200
 @@ -865,7 +865,7 @@
-                req->iface_flags = MBX_RX_IFACE_FLAGS_PROMISCUOUS;
-
-        if (enable & 0x02)
--               req->iface_flags = MBX_RX_IFACE_FLAGS_VLAN_PROMISCUOUS;
-+               req->iface_flags |= MBX_RX_IFACE_FLAGS_VLAN_PROMISCUOUS;
-
-        req->if_id = sc->if_id;
+ 		req->iface_flags = MBX_RX_IFACE_FLAGS_PROMISCUOUS;
+ 
+ 	if (enable & 0x02)
+-		req->iface_flags = MBX_RX_IFACE_FLAGS_VLAN_PROMISCUOUS;
++		req->iface_flags |= MBX_RX_IFACE_FLAGS_VLAN_PROMISCUOUS;
+ 
+ 	req->if_id = sc->if_id;
+ 
